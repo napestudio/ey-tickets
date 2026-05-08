21caf842a03bdb250191747e9c611a982f4f8d1c
cuerojr <cannibal5033@gmail.com>
2026-05-08 12:30:13 -0300

    minor fixes removed unnecesary files

--- init.sql
+++
@@ -1,7 +0,0 @@
--- Conectar a la base de datos creada por Docker
-\c ticketera;
-
--- Dar permisos totales al usuario postgres sobre el esquema public
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO postgres;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO postgres;
