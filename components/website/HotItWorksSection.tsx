6f5622f6316839883d20a0ac13b444da7418527a
Renzo Costarelli <renzo.costarelli@gmail.com>
2026-06-02 16:48:49 -0300

    update: landing page

---
+++ components/website/HotItWorksSection.tsx
@@ -0,0 +1,48 @@
+import HowItWorksCard from "./HowItWorksCard";
+import { Paragraph } from "./ui/Paragraph";
+import { Title } from "./ui/Title";
+
+const HOW_IT_WORKS_STEPS = [
+  {
+    title: "Creás tu cuenta",
+    text: "Creás tu cuenta en segundos.",
+  },
+  {
+    title: "Comprás los tickets",
+    text: "Comprás los tickets que necesites, los asignas a tus eventos y no tienen fecha de vencimiento.",
+  },
+  {
+    title: "Creás tu evento",
+    text: "Publicá tu evento y comenzá a vender. Los tickets que no se vendan los pódes usar para otro evento.",
+  },
+];
+
+export default function HowItWorksSection() {
+  return (
+    <section className="py-24 bg-linear-to-t to-black from-ey-turquoise-darker to-80%">
+      <div className="container mx-auto">
+        <div className="flex flex-col text-white ">
+          <Title className="font-base-neue font-bold">Cómo funciona?</Title>
+          <Paragraph>
+            Pagás un costo fijo por ticket. Sin costos ocultos, sin comisiones
+            por venta.
+          </Paragraph>
+        </div>
+
+        <div className="flex">
+          <div>
+            {HOW_IT_WORKS_STEPS.map((step, index) => (
+              <HowItWorksCard
+                key={index}
+                number={index + 1}
+                title={step.title}
+                text={step.text}
+              />
+            ))}
+          </div>
+          <div className="w-[600px] aspect-square bg-neutral-100"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- components/website/PricingSection.tsx
+++ components/website/PricingSection.tsx
@@ -1,31 +1,28 @@
 import { Title } from "./ui/Title";
 import ProductCard from "./ProductCard";
-
-const products = [
-  {
-    title: "Plan Básico",
-    description: "Ideal para equipos pequeños que recién arrancan.",
-    href: "/planes/basico",
-  },
-  {
-    title: "Plan Pro",
-    description: "Todo lo del básico más integraciones avanzadas.",
-    href: "/planes/pro",
-  },
-];
-
+import { PACKAGE_CATALOG } from "@/lib/ticket-stock-catalog";
+import PricePackageCard from "./PackageCard";
+import { Paragraph } from "./ui/Paragraph";
 export default function PricingSection() {
   return (
-    <section className="min-h-screen py-24 bg-ey-dark">
+    <section className="py-24 bg-linear-to-b to-ey-dark from-ey-turquoise-darker ">
       <div className="container mx-auto">
-        <Title>PRECIOS</Title>
-        <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {products.map((p) => (
-              <ProductCard key={p.title} {...p} />
-            ))}
-          </div>
+        <Title className="font-base-neue font-bold text-white mb-4">
+          PRECIOS
+        </Title>
+        <Paragraph className="text-white mb-12">
+          Pagás un costo fijo por ticket. Sin costos ocultos, sin comisiones por
+          venta, sin fecha de vencimiento.
+        </Paragraph>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 items-end pt-4">
+          {PACKAGE_CATALOG.map((p, index) => (
+            <PricePackageCard key={p.title} {...p} featured={index == 1} />
+          ))}
         </div>
+        <Paragraph className="text-white mt-12">
+          Podés comprar otras contidades y el precio se ajusta automaticamente.
+        </Paragraph>
       </div>
     </section>
   );
