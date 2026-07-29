57a5603eb7435a6fb073b03d3979269d2b325f63
Renzo Costarelli <renzo.costarelli@gmail.com>
2026-07-29 17:56:23 -0300

    wip: event page

--- components/event-header/event-header.tsx
+++ components/event-header/event-header.tsx
@@ -22,42 +22,42 @@ export default function EventHeader({
     <section className="w-full relative">
       <div className="w-200 max-w-[90vw] mx-auto p-6 md:py-12 md:px-10 z-10 relative">
         <div className="gap-6 flex flex-col mx-auto">
-          <div className="flex flex-col items-start justify-between gap-4">
+          <div className="space-y-2 w-[20rem] md:w-auto">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-4xl text-ey-turquoise">
+              {evento?.title}
+            </h1>
+            <div className="flex items-center gap-2 ">
+              <CalendarIcon className="w-4 h-4 shrink-0" />
+              <span className="text-sm font-medium">{dates}</span>
+            </div>
+            <div className="flex items-center gap-2 ">
+              <MapPin className="w-4 h-4 shrink-0 font-bold" />
+              <div>
+                {evento?.venue && (
+                  <span className="text-sm font-bold">{evento.venue} -</span>
+                )}
+                <span className="text-sm font-medium">{evento?.address}</span>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-5 items-start justify-between gap-4">
             {evento.image && (
               <div
-                className={`relative aspect-3/4 h-auto w-full overflow-hidden`}
+                className={`relative md:col-span-2 h-full w-full flex flex-col gap-4`}
               >
-                <Image
-                  src={evento?.image || ""}
-                  alt={evento.title || ""}
-                  fill
-                  className="object-cover"
-                />
+                <div
+                  className={`relative aspect-3/4 h-auto w-full overflow-hidden`}
+                >
+                  <Image
+                    src={evento?.image || ""}
+                    alt={evento.title || ""}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
               </div>
             )}
-            <div className="flex flex-col space-y-4 flex-1">
-              <div className="space-y-2 w-[20rem] md:w-auto">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-                  {evento?.title}
-                </h1>
-                <div className="flex items-center gap-2 ">
-                  <CalendarIcon className="w-4 h-4 shrink-0" />
-                  <span className="text-sm font-medium">{dates}</span>
-                </div>
-                <div className="flex items-center gap-2 ">
-                  <MapPin className="w-4 h-4 shrink-0 font-bold" />
-                  <div>
-                    {evento?.venue && (
-                      <span className="text-sm font-bold">
-                        {evento.venue} -
-                      </span>
-                    )}
-                    <span className="text-sm font-medium">
-                      {evento?.address}
-                    </span>
-                  </div>
-                </div>
-              </div>
+            <div className="flex flex-col md:col-span-3 space-y-4 flex-1">
               <div className="text-base text-balance">
                 <EventDescription html={evento?.description || ""} />
               </div>
